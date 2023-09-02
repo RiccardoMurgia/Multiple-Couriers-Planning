869945e2519b0eef5ea7009994bebc98c7b335b6
RiccardoMurgia <murgiariccardo96@gmail.com>
2023-09-02 21:49:14 +0200

    fixed some problems

--- Mcp.py
+++ Mcp.py
@@ -95,7 +95,8 @@ def solve_sat(config: 'dict', instances_path: 'str',
         print("model built, now solving...")
         solution = solver.split_search(timeout=config['timeout'], processes=config['processes'])
         print(solution)
-        json_parser.save_results('SAT',instance.name, solution, instance.max_load_indeces)
+        json_parser.save_results('SAT', instance.name, solution, instance.max_load_indeces)
+
 
 def solve_mip(config: 'dict', instances_path: 'str',
               instance_to_solve: Union[list[str], str] = 'all_instances'):
@@ -143,7 +144,7 @@ def solve_mip(config: 'dict', instances_path: 'str',
                 solver.solve(processes=config['processes'], timeout=config['timeout'])
                 result = solver.get_result()
 
-                json_parser.save_results('MIP', instance.name, result, sub_folders, instance.max_load_indeces)
+                json_parser.save_results('MIP', instance.name, result, instance.max_load_indeces, sub_folders)
                 print("<----------------------------------------------->")
                 print(f'solution for library {lib}:')
                 print(result)
@@ -188,7 +189,6 @@ def main(config: 'dict'):
     if 'mip' in models_to_use:
         print("============================================================================")
         solve_mip(config['mip'], config['instances_path'], instances_to_solve)
-        print('ooooooo')
     if 'smt' in models_to_use:
         print("============================================================================")
         solve_smt(config['smt'], config['instances_path'], instances_to_solve)
